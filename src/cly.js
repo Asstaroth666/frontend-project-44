676da1b30c32c764584b5a03df290c74c24fa7b8
Asstaroth666 <asstaroth6@gmail.com>
2024-02-08 08:37:09 +0300

    dsa

--- src/cly.js
+++ src/cly.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 
+
 const even = () => {
       console.log('brain-games');
   console.log('Welcome to the Brain Games! ');
@@ -8,20 +9,31 @@ const even = () => {
   );
   console.log(`Hello, ${name}!`);
 
+  
+  function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+    let randomNumber = getRandomArbitrary(1 , 100)
+
+
 console.log('Answer "yes" if the number is even, otherwise answer "no".'),
-  console.log('Question: 15')
+  console.log(`Question: ${randomNumber}`)
   const number = readlineSync.question(
     'Your Answer: '
     ) 
-
-    if (number == 'no') {
+    
+    if ('yes') {
         console.log('Correct!');
-    } else if (number == 'yes') {
+    } else if (randomNumber % 2 > 0 == 'no') {
         console.log (`'yes' is wrong answer ;(. Correct answer was 'no'.`); 
         console.log(`Let's try again, ${name}!`)
+        process.exit(0);
     } else {
         console.log('Wrong!')
+        process.exit(0);
     };
+
 
   console.log('Question: 24')
     const number2 = readlineSync.question(
@@ -33,22 +45,31 @@ console.log('Answer "yes" if the number is even, otherwise answer "no".'),
     } else if (number2 == 'no') {
         console.log (`'no' is wrong answer ;(. Correct answer was 'yes'.`); 
         console.log(`Let's try again, ${name}!`)
+        process.exit(0);
     } else {
         console.log('Wrong!')
+        process.exit(0);
     };
+
 
     console.log('Question: 73')
   const number3 =readlineSync.question(
     'Your Answer: '
     ) 
+
     if (number3 == 'no') {
         console.log('Correct!');
     } else if (number3 == 'yes') {
         console.log (`'yes' is wrong answer ;(. Correct answer was 'no'.`); 
         console.log(`Let's try again, ${name}!`)
+        process.exit(0);
     } else {
         console.log('Wrong!')
+        process.exit(0);
     };
 };
 
+
+
 export default even;
+
